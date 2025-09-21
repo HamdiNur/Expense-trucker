f7fbce93a8b799eab970e338cd56a6092a3849b2
HamdiNur <hamdoonur26@gmail.com>
2025-09-21 06:48:40 +0300

    Use checkUser server function to display logged-in user's name or Guest component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,19 @@
-const HomePage = () => {
-  return ( 
-  <main>
-   <h1>Expense Tracker</h1>
-  </main> );
+// app/page.tsx
+import AddTransaction from "./components/AddTransaction";
+import Guest from "./components/Guest";
+import { checkUser } from "@/lib/checkUser";
+
+export default async function HomePage() {
+  const user = await checkUser(); // <- ensures user is created in DB
+
+  if (!user) {
+    return <Guest />;
+  }
+
+  return (
+    <main>
+      <h1>Welcome, {user.name || "User"}</h1>
+      <AddTransaction/>
+    </main>
+  );
 }
- 
-export default HomePage;

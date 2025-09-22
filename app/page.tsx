cd92ef0d19e7e75043d27a76fdbe6c353c35f250
HamdiNur <hamdoonur26@gmail.com>
2025-09-22 09:59:38 +0300

    Update main page layout and package.json dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,10 @@
 // app/page.tsx
 import AddTransaction from "./components/AddTransaction";
+import Balance from "./components/Balance";
 import Guest from "./components/Guest";
 import { checkUser } from "@/lib/checkUser";
-
+import IncomeExpense from "./components/IncomeExpense";
+import TransactionList from "./components/TransactionList";
 export default async function HomePage() {
   const user = await checkUser(); // <- ensures user is created in DB
 
@@ -12,8 +14,12 @@ export default async function HomePage() {
 
   return (
     <main>
-      <h1>Welcome, {user.name || "User"}</h1>
+      <h2>Welcome, {user.name || "User"}</h2>
+     
+        <Balance/>
+        <IncomeExpense/>
       <AddTransaction/>
+      <TransactionList/>
     </main>
   );
 }
